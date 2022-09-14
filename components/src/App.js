62fb377ff915d08c7d3f943efcf6583b128a2eb0
AhmedRaza <86877851+ahmedrazachandio@users.noreply.github.com>
2022-09-14 23:21:21 +0500

    initial

---
+++ components/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Header from './components/herader';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ components/src/components/herader.js
@@ -0,0 +1,11 @@
+
+function Header() {
+  return (
+    <div className="gg">
+      <h1>Ahmed Raza</h1>
+      
+    </div>
+  );
+}
+
+export default Header;
